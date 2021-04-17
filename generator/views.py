4d913ca2b0e8fe999d89b850226f0b01359d4109
Mahfuzur Rashid <mmahfuz_rashid@yahoo.com>
2021-04-17 11:50:36 +1000

    Before Challenge

--- generator/views.py
+++ generator/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 # Create your views here.
 
@@ -8,6 +9,20 @@ def home(request):
 
 def password(request):
 
-thepassword = 'testing'
+    characters = list('abcdefghijklmnopqrstuvwxyz')
 
-return render(request, 'generator/password.html', {'password':thepassword})
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%^&*{}'))
+    if request.GET.get('numbers'):
+        characters.extend(list('0123456789'))
+
+    length = int(request.GET.get('length',12))
+
+    thepassword = ''
+    for x in range(length):
+        thepassword += random.choice(characters)
+
+
+    return render(request, 'generator/password.html', {'password':thepassword})
